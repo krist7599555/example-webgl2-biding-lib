09c7658fc035a5a4c1ccfe6cb2a074361744a422
krist7599555 <krist7599555@gmail.com>
2022-09-06 11:35:16 +0700

    feat: draw element

--- main.ts
+++ main.ts
@@ -32,47 +32,63 @@ const kgl = KrGl.create({
 
 const gl = kgl.gl;
 
+const { sin, cos, PI } = Math
+const TWOPI = 2 * PI
+const amp = 0.7;
+// prettier-ignore
+const vertecies = new Float32Array([
+  0.00000,0.00000,
+  sin(TWOPI * 0 / 5) * amp, cos(TWOPI * 0 / 5) * amp,
+  sin(TWOPI * 1 / 5) * amp, cos(TWOPI * 1 / 5) * amp,
+  sin(TWOPI * 2 / 5) * amp, cos(TWOPI * 2 / 5) * amp,
+  sin(TWOPI * 3 / 5) * amp, cos(TWOPI * 3 / 5) * amp,
+  sin(TWOPI * 4 / 5) * amp, cos(TWOPI * 4 / 5) * amp,
+  sin(TWOPI * 5 / 5) * amp, cos(TWOPI * 5 / 5) * amp,
+  
+])
+
+// prettier-ignore
+const indices = new Uint8Array( [
+  0,1,2,
+  0,2,3,
+  0,3,4,
+  0,4,5,
+  0,5,1,
+]);
+
 const apos = kgl.attribute_location("aPosition", "vec2");
 const acolor = kgl.attribute_location("aColor", "vec3");
 const umvp = kgl.uniform_location("uMvp", "mat4");
 
 // set default value
-apos.disable_attr_array().set_attr_data_fallback({ data: [0, 0] });
-acolor.disable_attr_array().set_attr_data_fallback({ data: [1, 0, 1] });
+// apos.disable_attr_array().set_attr_data_fallback({ data: [0, 0] });
+acolor.disable_attr_array().set_attr_data_fallback({ data: [1, 0, 0] });
 // set vertex array
 
-const count = 3;
 
 KrGlBuffer.create("ARRAY_BUFFER")
-  .data(
-    new Float32Array(
-      [
-        [0, 1, 1, 1, 0],
-        [-1, -1, 0, 1, 1],
-        [1, -1, 1, 0, 1],
-      ].flat()
-    )
-  )
+  .data(vertecies)
   .bind(() => {
-    const sz = [
-      TYPECONVERT[apos.type].size_in_byte,
-      TYPECONVERT[acolor.type].size_in_byte,
-    ];
     apos
       .enable_attr_array()
-      .set_attr_to_active_buffer({ strip: sum(sz), offset: sum(sz.slice(0, 0)) });
-    acolor
-      .enable_attr_array()
-      .set_attr_to_active_buffer({ strip: sum(sz), offset: sum(sz.slice(0, 1)) });
+      .set_attr_to_active_buffer();
+  });
+
+KrGlBuffer.create("ELEMENT_ARRAY_BUFFER")
+  .data(indices)
+  .bind(() => {
+    
   });
 
 requestAnimationFrame(function f(t) {
   umvp.set_uniform_data({
-    data: [false, Matrix4.IDENTITY.clone().rotateZ(t / 100)],
+    data: [false, Matrix4.IDENTITY.clone().rotateZ(t / 500)
+  ],
   });
 
   gl.clearColor(0.72, 0.83, 0.93, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, count);
+  // gl.drawArrays(gl.TRIANGLES, 0, count);
+  gl.drawElements(gl.TRIANGLES, 15, gl.UNSIGNED_BYTE, 0)
   requestAnimationFrame(f);
 });
